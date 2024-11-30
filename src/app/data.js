9ccb35a35e9ad0a4e388dfbce73a329e3e8444b2
Abhishek Kumar <110620295+Abhishek99M@users.noreply.github.com>
2024-11-30 19:30:10 +0530

    file upload

---
+++ src/app/data.js
@@ -0,0 +1,117 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "AI-TRIP-PLANNER",
+    description: "Built an AI travel planner with itinerary creation and real-time location data using React.js, Firebase, and Google Places API.",
+    date: "2022-08-15",
+    demoLink: "https://ai-travel-planner-kappa.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Weather-APP",
+    description: "A responsive weather app that provides real-time weather updates and forecasts for any location.",
+    date: "2022-08-15",
+    demoLink: "https://weather-app-six-phi-62.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Rock-Paper-Scissor",
+    description: "An interactive Rock-Paper-Scissors game built with JavaScript, providing a fun and dynamic user experience against a computer opponent.",
+    date: "2022-06-20",
+    demoLink: "https://github.com/Abhishek99M/Rock-Paper-Scissor",
+  },
+  {
+    id: 4,
+    name: "Personal Portfolio Website",
+    description: "Built an interactive, responsive portfolio showcasing projects with 3D visuals, animations, and contact integration.",
+    date: "2022-11-05",
+    demoLink: "https://my-portfolio-tau-nine-54.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "Tic-Tac-Toe",
+    description: "A classic Tic-Tac-Toe game built with HTML, CSS, and JavaScript, allowing two players to enjoy a simple, interactive gameplay experience.",
+    date: "2022-09-10",
+    demoLink: "https://github.com/Abhishek99M/TicTacToe_Project",
+  },
+  {
+    id: 6,
+    name: "Snake-Game",
+    description: "A classic Snake Game built in Python with a user-friendly GUI.",
+    date: "2022-05-30",
+    demoLink: "https://github.com/Abhishek99M/Snake-Game",
+  },
+  {
+    id: 7,
+    name: "Sentiment-Analysis",
+    description: "A Python-based Sentiment Analysis project that evaluates text sentiment to determine positive, negative, or neutral tones.",
+    date: "2022-07-12",
+    demoLink: "https://github.com/Abhishek99M/Sentiment_Analysis_Project",
+  },
+  {
+    id: 8,
+    name: "Text-Classification",
+    description: "A Text Classification project using Naive Bayes in Python to categorize text data efficiently.",
+    date: "2022-10-01",
+    demoLink: "https://github.com/Abhishek99M/Text_classification_NB",
+  },
+  {
+    id: 9,
+    name: "CurrencyConverter",
+    description: "A Currency Converter app that converts amounts between currencies instantly.",
+    date: "2022-04-18",
+    demoLink: "https://currency-converter-zeta-pied.vercel.app/",
+  },
+    
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/Abhishek99M",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/abhishek-kumar-a04701233/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://x.com/Abhishek_M_2004",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/Abhi_Resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+return (
+<section className="py-20 w-full">
+    <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+    <ItemLayout
+        className={
+        " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+        }
+    >
+        <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+        Architect of Enchantment
+        </h2>
+        <p className="font-light  text-xs sm:text-sm md:text-base   ">
+        My journey in web development is powered by an array of mystical
+        tools and languages, with JavaScript casting the core of my
+        enchantments. I use frameworks like React.js and Next.js with
+        precision, crafting seamless portals (websites) that connect 
+        users across the digital universe. I am always eager to explore new 
+        technologies and push the boundaries of web development.
+        Whether it's optimizing performance or improving user experience, 
+        I am committed to delivering top-tier solutions. The ancient arts of the
+        Jamstack empower me to create fast, secure, and dynamic experiences,
+        while my design skills ensure every creation is not only functional
+        but visually captivating. Join me as I continue to explore new 
+        technologies to shape the future of the web.
+        </p>
+    </ItemLayout>
+
+    <ItemLayout
+        className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+    >
+        <p className="font-light  text-xs sm:text-sm md:text-base">
+        Proficient in React.js and JavaScript, with Experience in Web Technologies & Data Science Projects. 
+        </p>
+    </ItemLayout>
+
+    <ItemLayout
+        className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+    >
+        <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+        0-1 {" "}
+        <sub className="font-semibold text-base">years of experience</sub>
+        </p>
+    </ItemLayout>
+
+    <ItemLayout
+        className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}>
+        <img
+        className="w-full h-auto"
+        src={`https://github-readme-stats.vercel.app/api/top-langs/?username=Abhishek99M&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+        alt="Abhishek99M"
+        loading="lazy"
+        />
+        
+    </ItemLayout>
+    
+    <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+        <img
+        className="w-full h-auto"
+        src={`https://github-readme-stats.vercel.app/api/?username=Abhishek99M&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+        alt="Abhishek99M"
+        loading="lazy"
+        />
+    </ItemLayout>
+    
+    <ItemLayout className={"col-span-full"}>
+        <img
+        className="w-full h-auto"
+        src={`https://skillicons.dev/icons?i=java,py,aws,github,html,ai,js,linkedin,mysql,nextjs,react,tailwind,vscode,css,nodejs`}
+        alt="Abhishek99M"
+        loading="lazy"
+        />
+    </ItemLayout>
+
+    <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+        <img className="w-full h-auto" src={"https://github-readme-streak-stats.herokuapp.com?user=Abhishek99M&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B"} 
+        alt="Abhishek99M" loading="lazy"
+        />
+    </ItemLayout>
+
+    <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+        <Link
+        href="https://github.com/Abhishek99M/Weather_APP"
+        target="_blank"
+        className="w-full"
+        >
+        <img
+            className="w-full h-auto"
+            src={`https://github-readme-stats.vercel.app/api/pin/?username=Abhishek99M&repo=Weather_APP&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+            alt="Abhishek99M"
+            loading="lazy"
+        />
+        </Link>
+    </ItemLayout>
+    </div>
+</section>
+);
+};
+
+export default AboutDetails;
